96b1df90dde54e2caff8afc10d0982e2a5586a23
tkr231 <t.s.koster@vu.nl>
2022-07-28 10:45:12 +0200

    fix: column name fix

--- R/fill_missing.R
+++ R/fill_missing.R
@@ -166,16 +166,18 @@ fill_missing <- function(x, min_known_n = NULL, min_known_p = NULL, type) {
 #' @importFrom dplyr %>%
 #' @importFrom rlang :=
 #' @export
-fill_with_agg_by_group <- function(df, group, columns, overwrite_col = FALSE, statistic = mean, fill_empty_group = FALSE){
+fill_df_with_agg_by_group <- function(df, group, columns, overwrite_col = FALSE, statistic = mean, fill_empty_group = FALSE){
   ## Fills missing values with summary statistics (mean, median, vvconverter::mode, etc.) per group
   new_cols <- columns %>% purrr::map_dfc(function(col){
     col_output = ifelse(overwrite_col, col, paste0(col, "_imputed"))
-    df <- df %>%
-      fill_col_with_agg_by_group(group, col, col_output, statistic)
+
+    df[col_output] <- df %>%
+      fill_col_with_agg_by_group(group = group, col = col, statistic = statistic)
     if (fill_empty_group){
-      df <- df %>%
-        fill_col_with_agg_by_group(group=c(), col, col_output, statistic)
+      df[col_output] <- df %>%
+        fill_col_with_agg_by_group(group = c(), col = col_output, statistic = statistic)
     }
+
     return(df %>% dplyr::select(col_output))
   })
   ## Add updated columns to df
@@ -190,28 +192,26 @@ fill_with_agg_by_group <- function(df, group, columns, overwrite_col = FALSE, st
 
 #' Fill column with aggregate by group
 #'
-#' Function for use in fill_with_agg_by_group(). Could be used by itself, but adds no value over
-#' fill_with_agg_by_group().
-#'
 #' Calculate a summary statistic (mean, median, vvconverter::mode, min, max etc.) by group
-#' and use it to fill missing values in a single column. Note: this takes and produces a tibble rather than a vector.
+#' and use it to fill missing values in a column. Primarily for use in fill_with_agg_by_group().
 #'
 #' @param df tibble to use
 #' @param group string or vector of strings: columns to group by
 #' @param col string: column to impute
-#' @param col_output string: column name of new imputed column
 #' @param statistic function: summary statistic to use (mean, median, min etc.). For now requires a function with na.rm argument
-#' @return a tibble with filled column
+#' @return a filled vector
 #' @importFrom dplyr %>%
 #' @importFrom rlang :=
 #' @export
-fill_col_with_agg_by_group <- function(df, group, col, col_output, statistic){
+fill_col_with_agg_by_group <- function(df, group, col, statistic){
+  new <- NULL
   ## Fills missing values with summary statistics (mean, median, vvconverter::mode, etc.) per group
-  df <- df %>%
+  result_column <- df %>%
     dplyr::group_by(!!!dplyr::syms(group)) %>%
-    dplyr::mutate({{col_output}} := ifelse(is.na(!!dplyr::sym(col)),
-                                           statistic(!!dplyr::sym(col), na.rm = T),
-                                           !!dplyr::sym(col))) %>%
-    dplyr::ungroup()
-  return(df)
+    dplyr::mutate(new = ifelse(is.na(!!dplyr::sym(col)),
+                               statistic(!!dplyr::sym(col), na.rm = T),
+                               !!dplyr::sym(col))) %>%
+    dplyr::ungroup() %>%
+    dplyr::pull(new)
+  return(result_column)
 }
