5e8accee96e2411c211f1c9f80416673410e30ca
Tomer Iwan <t.iwan@vu.nl>
2022-09-16 10:49:50 +0200

    translate: function comments

--- R/fill_missing_rownumber.R
+++ R/fill_missing_rownumber.R
@@ -1,34 +1,31 @@
 #' Fill missing rownumber
 #'
-#' Een functie om missende waarden van een telvariabele op te vullen. De ontbrekende
-#' missende waarden worden opgevuld door door te tellen vanaf de laatst bekende
-#' waarde.
-#' c(NA,4,NA,NA) wordt bijvoorbeeld c(NA,4,5,6)
-#'
-#' @param x een vector met integers
-#' @return x, met de NA's opgevuld indien mogelijk
-#' @family vector berekeningen
-#' @family missing data functies
+#' Impute missing values of a count variable. Imputation is done by counting from
+#' the last known value. Example: c(NA,4,NA,NA) then becomes c(NA,4,NA,NA).
+#' @param x Integer vector.
+#' @return Integer vector with filled values.
+#' @family vector calculations
+#' @family missing data functions
 #' @export
 #' @examples fill_missing_rownumber(c(NA,4,NA,NA))
 fill_missing_rownumber <- function(x){
-    ## Als er geen bekende waarden in x zitten kan de functie niet opvullen
+    ## Check if vector has missing values
     if (length(stats::na.omit(x)) == 0) {
         return(x)
     }
-    ## Als x niet een numeric is kan niet opgevuld worden
+    ## Check if vector is of class numeric
     if (!is.numeric(x)) {
         return(x)
     }
 
-    ## Bepaal de positie van de maximale waarde
+    ## Determine index of max value
     position_max <- match(max(x, na.rm = T), x)
     length_x <- length(x)
-    ## Voer onderstaande alleen uit als de maximale waarde niet de laatste waarde is
+    ## If the last known value is not the max the if statement is triggered.
     if (position_max < length_x) {
-        ## Bepaal de posities van missende waarden die opgevuld kunnen worden
+        ## Determine the positions of the missing values that can be imputed.
         position_missing <- (position_max + 1):length_x
-        ## vul die posities op, door door te tellen
+        ## Impute by counting up.
         x[position_missing] <- x[position_max] + position_missing - position_max
     }
     return(x)
